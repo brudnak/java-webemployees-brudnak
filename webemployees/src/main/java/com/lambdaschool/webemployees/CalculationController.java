c95e1c3178cf156a3f47e10e0a60af48d3b04489
Andrew Brudnak <brudnak@icloud.com>
2019-07-14 15:53:50 -0700

    :building_construction: Calculation controller fix

--- webemployees/src/main/java/com/lambdaschool/webemployees/CalculationController.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/CalculationController.java
@@ -1,7 +1,8 @@
 package com.lambdaschool.webemployees;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,15 @@ public class CalculationController {
 
     // localhost:2019/calc/salary/1/0.05
     // return how much the employee would get
-    @Mapping(value = "/salary/{id}/{raise}",
+    @GetMapping(value = "/salary/{id}/{raise}",
                 produces = {"application/json"})
     public ResponseEntity<?> checkRaise(
             @PathVariable
                     long id,
             @PathVariable
                     double raise) {
-
+        Employee tempEmp = new Employee(WebemployeesApplication.ourEmpList.findEmployee(e->(e.getId() == id)));
+        tempEmp.setSalary(tempEmp.getSalary() * (1.0 + raise));
+        return new ResponseEntity<>(tempEmp, HttpStatus.OK);
     }
 }
