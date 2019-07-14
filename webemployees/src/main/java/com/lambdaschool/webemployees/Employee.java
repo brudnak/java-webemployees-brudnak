bda1beedee9ea22167481ae657f72e30c292ae6d
Andrew Brudnak <brudnak@icloud.com>
2019-07-14 10:29:12 -0700

    :paperclip: Setting up the Employee class fields

--- webemployees/src/main/java/com/lambdaschool/webemployees/Employee.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/Employee.java
@@ -1,4 +1,21 @@
 package com.lambdaschool.webemployees;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Employee {
+
+    // fields
+
+    // AtomicLong takes account for possible collisions
+    // if multiple points are trying to create employees at
+    // the same time. Handles concurrency issues.
+
+    private static final AtomicLong counter = new AtomicLong();
+    private long id;
+    private String fname;
+    private String lname;
+    private double salary;
+    private boolean has401k;
+    private int companyID;
+    private int healthPlanID;
 }
