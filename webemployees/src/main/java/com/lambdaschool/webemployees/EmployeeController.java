136ca2358e008d07ae57e4ea73fa436c0ee51540
Andrew Brudnak <brudnak@icloud.com>
2019-07-14 11:43:10 -0700

    :umbrella: Get all employees working port 2019

--- webemployees/src/main/java/com/lambdaschool/webemployees/EmployeeController.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/EmployeeController.java
@@ -1,4 +1,19 @@
 package com.lambdaschool.webemployees;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/data")
 public class EmployeeController {
+
+    // localhost:2019/data/allemployees
+    @RequestMapping(value = "/allemployees",
+                    produces = {"application/json"})
+    public ResponseEntity<?> getAllEmployees() {
+        WebemployeesApplication.ourEmpList.empList.sort((e1, e2) -> e1.getLname().compareToIgnoreCase(e2.getLname()));
+        return new ResponseEntity<>(WebemployeesApplication.ourEmpList.empList, HttpStatus.OK);
+    }
 }
