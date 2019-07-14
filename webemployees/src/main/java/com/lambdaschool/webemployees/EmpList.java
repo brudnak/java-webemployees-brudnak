39a8ca860b49e9d53914adce5920864baa626f72
Andrew Brudnak <brudnak@icloud.com>
2019-07-14 12:02:46 -0700

    :shaved_ice: Methods added to EmpList

--- webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
@@ -11,4 +11,46 @@ public class EmpList {
         empList.add(new Employee("Sam", "Ford", 80000, true, 1, 1));
         empList.add(new Employee("John", "Jones", 75000, true, 1, 1));
     }
-}
+
+    // ******************************************
+    // METHOD - findEmployee
+    // takes SINGLE PARAMETER - will be Lambda Expression
+    public Employee findEmployee(CheckEmployee tester) {
+
+        // loop through all employees
+        for (Employee e: empList){
+
+            // check if lambda expression is true
+            if (tester.test(e)) {
+                // return the employee that was just found
+                return e;
+            }
+        }
+        // if no matching employee is found return null
+        return null;
+    } // END OF METHOD
+    // ******************************************
+
+    // ******************************************
+    // METHOD - findEmployees - PLURAL
+    // takes SINGLE PARAMETER - will be Lambda Expression
+    public ArrayList<Employee> findEmployees(CheckEmployee tester) {
+
+        // creating TEMPORARY array list
+        ArrayList<Employee> tempEmpList = new ArrayList<>();
+
+        // loop though employees
+        for (Employee e: empList) {
+
+            // check if lambda expression is true
+            if (tester.test(e)) {
+
+                // if true, adds to temporary list
+                tempEmpList.add(e);
+            }
+        }
+
+        return tempEmpList;
+    } // END OF METHOD
+    // ******************************************
+} // END OF CLASS
